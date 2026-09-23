7b214981a577f0d534819c4fd2bd9777ad156efa
ihsandulu <ihsan.dulu@gmail.com>
2026-09-23 11:29:31 +0700

    perbaikan simpan token

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1564,16 +1564,18 @@ class api extends CI_Controller
 		if ($this->input->get("tipe") == "siswa") {
 			$inputuser["user_token"] = $this->input->get("token");
 			$where["user_nisn"] = $this->input->get("nisn");
+			$this->db->update("user", $inputuser, $where);
 		}
 		if ($this->input->get("tipe") == "guru") {
 			$inputuser["user_tokenguru"] = $this->input->get("token");
 			$where["user_nik"] = $this->input->get("nik");
+			$this->db->update("user", $inputuser, $where);
 		}
 		if ($this->input->get("tipe") == "walimurid") {
 			$inputuser["user_tokenortu"] = $this->input->get("token");
 			$where["user_nisn"] = $this->input->get("nisn");
+			$this->db->update("user", $inputuser, $where);
 		}
-		$this->db->update("user", $inputuser, $where);
 	}
 
 	public function ambilpesan()
