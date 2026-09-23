a9c70f2df03eea9507cefebe8ae0bceadab4f059
ihsandulu <ihsan.dulu@gmail.com>
2026-09-23 22:05:24 +0700

    ambil pesan update API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1638,8 +1638,9 @@ class api extends CI_Controller
 				$tipe = $this->input->get("tipe");
 				$statuspesan = "Gagal kirim notif ";
 				//delete pesan nya				
-				/* $this->db->where("pesan_id", $pesan_id);
-				$this->db->delete("pesan"); */
+				$this->db->where("pesan_id", $pesan_id);
+				$this->db->delete("pesan");
+				echo $readdata = $this->db->last_query();
 			}
 			// }
 		}
